6284709ca5754243bea2a537b16ca1bed1a9640e
James Anwyl <james.anwyl@parall.ax>
2022-09-25 04:02:54 +0100

    Refactor userprofile tests

--- src/components/UserProfile.spec.jsx
+++ src/components/UserProfile.spec.jsx
@@ -5,77 +5,73 @@ import { AuthContextProvider } from '../context/authContext';
 import UserProfile from './UserProfile';
 
 describe('User profile', () => {
-  beforeEach(() => {
-    // This component uses 2 contexts: authContext which is wrapped around <UserProfile> in app.js, and colorContext which is global and wrapped around all components in app.js
-    // Without access to the contexts, the test would fail
-    // The render method we use here is overriden by a custom method thats defined in test-utils.js. You can add any global context providers there (as we've done with ColorContextProvider)
-    render(
-      <AuthContextProvider>
-        <UserProfile />
-      </AuthContextProvider>
-    );
-  });
-  it('Login button is visible when user is logged out', async () => {
-    const loginBtn = screen.getByRole('button', {
-      name: /log in/i,
-    });
-    expect(loginBtn).toBeVisible();
-  });
+    beforeEach(() => {
+      // set auth token
+      global.localStorage.setItem('isLoggedIn', "1")
 
-  it('Login button is not visible when user is logged in', async () => {
-    const loginBtn = screen.getByRole('button', {
-      name: /log in/i,
+      // This component uses 2 contexts: authContext which is wrapped around <UserProfile> in app.js, and colorContext which is global and wrapped around all components in app.js
+      // Without access to the contexts, the test would fail
+      // The render method we use here is overriden by a custom method thats defined in test-utils.js. You can add any global context providers there (as we've done with ColorContextProvider)
+      render(
+        <AuthContextProvider>
+          <UserProfile />
+        </AuthContextProvider>
+      );
     });
 
-    await userEvent.click(loginBtn);
-    expect(loginBtn).not.toBeInTheDocument();
-  });
+    afterEach(() => {
+      global.localStorage.clear()
+    })
 
-  it('Logout button is visible when user is logged in', async () => {
-    const loginBtn = screen.getByRole('button', {
-      name: /log in/i,
-    });
+    it('User is logged in automatically if auth token is already set', async () => {
+      // Check that logged in text and logout button are visible
+      const loggedInTxt = screen.getByText('You are logged in');
+      const logoutBtn = screen.getByRole('button', {
+        name: /log out/i,
+      });
+      expect(loggedInTxt).toBeVisible();
+      expect(logoutBtn).toBeVisible();
 
-    await userEvent.click(loginBtn);
+      // Check that logged out text and login button are not visible
+      const loggedOutTxt = screen.queryByText('You are logged out')
+      const loginBtn = screen.queryByRole('button', {
+        name: /log in/i
+      })
+      expect(loggedOutTxt).not.toBeInTheDocument()
+      expect(loginBtn).not.toBeInTheDocument()
+    })
 
-    const logoutBtn = screen.getByRole('button', {
-      name: /log out/i,
-    });
-    expect(logoutBtn).toBeVisible();
-  });
+    it('User is logged out successfully when logout button is clicked', async () => {
+      // Click the logout button
+      const logoutBtn = screen.getByRole('button', {
+        name: /log out/i,
+      });
+      await userEvent.click(logoutBtn)
 
-  it('Logout button is not visible when user is logged out', async () => {
-    const logoutBtn = screen.queryByRole('button', {
-      name: /log out/i,
-    });
-    expect(logoutBtn).not.toBeInTheDocument();
-  });
+      // Check that logged out text and login button are visible
+      const loggedOutTxt = screen.getByText('You are logged out')
+      const loginBtn = screen.getByRole('button', {
+        name: /log in/i
+      })
+      expect(loggedOutTxt).toBeVisible() 
+      expect(loginBtn).toBeVisible() 
+    })
 
-  it('Correct helper text is displayed when user is logged in or logged out', async () => {
-    const loggedOutTxt = screen.getByText('You are logged out');
-    expect(loggedOutTxt).toBeVisible();
+    it('User is logged in successfully when login button is clicked', async () => {
+      // Click the logout button
+      const logoutBtn = screen.getByRole('button', {
+        name: /log out/i,
+      });
+      await userEvent.click(logoutBtn)
 
-    const loginBtn = screen.getByRole('button', {
-      name: /log in/i,
-    });
-    userEvent.click(loginBtn);
-
-    const loggedInTxt = await screen.findByText('You are logged in');
-    expect(loggedInTxt).toBeVisible();
-  });
-
-  it('User is logged out successfully when log out button is pressed', async () => {
-    const loginBtn = screen.getByRole('button', {
-      name: /log in/i,
-    });
-    userEvent.click(loginBtn);
-
-    const logoutBtn = await screen.findByRole('button', {
-      name: /log out/i,
-    });
-    await userEvent.click(logoutBtn);
-
-    const loggedOutTxt = screen.getByText('You are logged out');
-    expect(loggedOutTxt).toBeVisible();
-  });
+      // Click the login button
+      const loginBtn = screen.getByRole('button', {
+        name: /log in/i
+      })
+      await userEvent.click(loginBtn)
+      
+      // Check that logged in text is visible 
+      const loggedInTxt = screen.getByText('You are logged in');
+      expect(loggedInTxt).toBeVisible();
+    })
 });
