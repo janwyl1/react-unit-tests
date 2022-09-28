618076bf6cdefe7fa4cee68ece38e6fc778c6f6f
James Anwyl <james.anwyl@parall.ax>
2022-09-28 04:58:12 +0100

    Update test descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         </AuthContextProvider>
         <hr />
         <h2>Example 4 - Custom hook</h2>
-        <p>Uses renderHook and act to test a custom hook</p>
+        <p>Uses renderHook and act() to test a custom hook</p>
         <p>This custom hook isn't used in any components</p>
         <p>See: useCounter.js / useCounter.spec.js</p>
         <hr />
